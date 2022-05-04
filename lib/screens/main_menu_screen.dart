2df7e0b3edffaf475a1ee8a1a6fb46724fd3af86
fwdAndroid <fwdkaleem@gmail.com>
2022-05-04 09:37:35 +0500

    Colors Integration with Project Creation

---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatefulWidget {
+  const MainMenuScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<MainMenuScreen> createState() => _MainMenuScreenState();
+}
+
+class _MainMenuScreenState extends State<MainMenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
