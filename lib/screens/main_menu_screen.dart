19f09bbaf8eabf0cf88377eea67c03c5feea95d7
fwdAndroid <fwdkaleem@gmail.com>
2022-05-04 09:48:33 +0500

    Implementing Custom Button Widgets

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/widgets/custom_buttons.dart';
 
 class MainMenuScreen extends StatefulWidget {
   const MainMenuScreen({ Key? key }) : super(key: key);
@@ -10,8 +11,16 @@ class MainMenuScreen extends StatefulWidget {
 class _MainMenuScreenState extends State<MainMenuScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomButtons(onTap: (){}, text: 'Create Room'),
+          SizedBox(height: 15,),
+                    CustomButtons(onTap: (){}, text: 'Join Room'),
+
+        ],
+      ),
     );
   }
 }
