249546c483b3b7ecfb77dd2c8991a5821bd59519
fwdAndroid <fwdkaleem@gmail.com>
2022-05-04 09:55:17 +0500

    Adding Box Shadow

--- lib/widgets/custom_buttons.dart
+++ lib/widgets/custom_buttons.dart
@@ -9,14 +9,25 @@ CustomButtons({required this.onTap,required this.text});
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
-    return ElevatedButton(
-      onPressed: onTap,
-      child: Text(text),
-      style: ElevatedButton.styleFrom(
-        minimumSize: Size(width/5, 50)
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.blue,
+            blurRadius:5,
+            spreadRadius: 0
+          )
+        ]
       ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        child: Text(text),
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(width/5, 50)
+        ),
 
-      
+        
+      ),
     );
   }
 }
