e67c151c45f4f68e6664b2b52ab8433c32e20558
fwdAndroid <fwdkaleem@gmail.com>
2022-05-04 12:20:50 +0500

    Mongo DB and Socket IO Connection Established

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 //Importing 
+const { Socket } = require("dgram");
 const express = require("express");
 const http = require("http");
 
@@ -8,9 +9,24 @@ var server = http.createServer(app);
 
 var io = require("socket.io")(server);
 
+const mongoose = require("mongoose");
+
 // Middle Ware use to transfer data from client to server side
 app.use(express.json());
+
+//Establish Connection
+const DB = "mongodb+srv://fwdkaleem:zeo123456@cluster0.ejioi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(DB).then(() => {
+    console.log("Connection Established");
+}).catch((e) => {
+    console.log(e);
+});
+//Socket Connection
+io.on('connection',(socket) => {
+    console.log("connect");
+})
 
 server.listen(port,'0.0.0.0',() => {
     console.log(`server started ${port}`)
 });
+
