ddc1422e52d4f8f02a52b8b346bf2caadbcc38fb
fwdAndroid <fwdkaleem@gmail.com>
2022-05-04 09:58:20 +0500

    Create Responsive Widget

---
+++ lib/responsiveness/responsive.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Responsive extends StatelessWidget {
+
+final Widget child;
+Responsive({required this.child});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ConstrainedBox(constraints: BoxConstraints(
+        maxWidth:600,
+        
+      ),child: child,),
+      
+    );
+  }
+}
