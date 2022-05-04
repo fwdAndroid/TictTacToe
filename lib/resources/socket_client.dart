d486875ceab9258ec59fc3d1090bed142bb7eda4
fwdAndroid <fwdkaleem@gmail.com>
2022-05-04 12:47:07 +0500

    Socket Singleton Class

---
+++ lib/resources/socket_client.dart
@@ -0,0 +1,19 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketClient{
+  IO.Socket? socket;
+  static SocketClient? _instance;
+
+  SocketClient._instnace(){
+    socket = IO.io('http://192.168.250.24:3000',<String,dynamic> {
+      'transports' : ['websocket'],
+      'autoConnect':false
+    });
+    socket!.connect();
+  }
+
+   static SocketClient get instance {
+    _instance ??= SocketClient._instnace();
+    return _instance!;
+  }
+}
